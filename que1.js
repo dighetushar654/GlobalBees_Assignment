0f15ab4aaeef9f1558fa63cee1d7f2938992d8f9
Tushar Dighe <dighetushar654@gmail.com>
2021-10-08 16:47:45 +0530

    update

---
+++ que1.js
@@ -0,0 +1,22 @@
+// - Write a function that finds all positive numbers in the integer array that have their opposites in it as well.
+    
+    // Eg:  [-9, 4, -10, 15, 19, -8, -2, 3, 9, -15, 10, -20]
+    
+    // Output : [-15, 15], [-10, 10], [-9, 9]
+
+    function oppositeNumbers(arr) {
+        for(let i = 0; i<arr.length; i++){
+            for(let j= 0; j<arr.length; j++){
+                if(Math.sign(arr[j]) == -1){
+                    if(Math.sign(arr[i]) == 1 &&  Math.sign(arr[j]) == -1){
+                        if(arr[i] == Math.abs(arr[j])){
+                             console.log(arr[i], arr[j])
+                        }
+                    }
+                }
+            }
+        }
+    }
+const arr = [-9, 4, -10, 15, 19, -8, -2, 3, 3, 9, -15, 10, -20];
+oppositeNumbers(arr);
+
---
+++ que2.js
@@ -0,0 +1,3 @@
+// - Let’s assume that M is a very large number. Find the number of ways in which you can traverse M meters by doing jumps of 6, 7, 8, 9, or 10 meter lengths.
+// - What is the time complexity of that algorithm?
+
